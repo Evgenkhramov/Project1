495eb89e8e89e9d389c3a699c73a7fe222dd546c
Yvgen Khramov <Evgenkhramov@gmail.com>
2018-12-28 16:19:24 +0200

    Dictionary ввел

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -13,27 +13,50 @@ namespace ConsoleApp18
         Diamonds = 3,
         Clubs = 4
     }
-    public enum CardNumber
-    {
-        Ace = 11,
-        King = 10,
-        Queen = 10,
-        Jack = 10,
-        Ten = 10,
-        Nine = 9,
-        Eight = 8,
-        Seven = 7,
-        Six = 6,
-        Five = 5,
-        Four = 4,
-        Three = 3,
-        Two = 2
-    }
+
+    //public enum CardNumber
+    //{
+    //    Ace = 11,
+    //    King = 10,
+    //    Queen = 10,
+    //    Jack = 10,
+    //    Ten = 10,
+    //    Nine = 9,
+    //    Eight = 8,
+    //    Seven = 7,
+    //    Six = 6,
+    //    Five = 5,
+    //    Four = 4,
+    //    Three = 3,
+    //    Two = 2
+    //}
     public enum GamerStatus
     {
         Win = 1,
         Lost = 2,
         Plays = 3
+    }
+    public class Cards
+    {
+        public int Points;
+        public string CardName;
+        public static Dictionary<string, int> CardPointDict = new Dictionary<string, int>
+        {
+            { "Ace",11},
+            { "King",10},
+            { "Queen",10},
+            { "Jack",10},
+            { "Ten",10},
+            { "Nine",9},
+            { "Eight",8},
+            { "Seven",7},
+            { "Six",6},
+            { "Five",5},
+            { "Four",4},
+            { "Three",3},
+            { "Two",2}
+
+        };
     }
 
     public class Card
@@ -54,12 +77,12 @@ namespace ConsoleApp18
             var cards = new List<Card>(52);
             foreach (var suitName in Enum.GetNames(typeof(Suit)))
             {
-                foreach (var cardNumber in Enum.GetNames(typeof(CardNumber)))
+                foreach (KeyValuePair<string, int> keyValue in Cards.CardPointDict)
                 {
                     cards.Add(new Card
                     {
                         Suit = suitName,
-                        CardNumber = cardNumber
+                        CardNumber = keyValue.Key
                     });
                 }
             }
@@ -69,10 +92,10 @@ namespace ConsoleApp18
 
         public static Card TakeSomeCard(List<Card> NewDeck)
         {
-            
+
             int IndexCard = CardRandomIndex(NewDeck.Count);
             if (IndexCard >= 0)
-            { 
+            {
                 var SomeCard = NewDeck[IndexCard];
                 NewDeck.RemoveAt(IndexCard);
                 return SomeCard;
@@ -85,47 +108,50 @@ namespace ConsoleApp18
             if (DeckLength > 0)
             {
                 Random rnd = new Random();
-                int value = rnd.Next(0,DeckLength);
+                int value = rnd.Next(0, DeckLength);
                 return value;
             }
             else
                 return 0;
         }
-        public static int CardPoints(Card SomeCard)
-        {
-            string thisCard = SomeCard.CardNumber;
-            CardNumber OneCard = (CardNumber)Enum.Parse(typeof(CardNumber), thisCard);
-            int Points = (int)OneCard;
-            return Points;
-        }
+
     }
 
     public class Gamer
     {
-        public string Name;
-        public int Points;
-        public 
+        public int GamerIndex;
+        public string GamerName;
+        public int GamerPoints;
+        public GamerStatus Status;
     }
+
 
     class Program
     {
 
         static void Main(string[] args)
         {
-            //int someI = 0;
-            var newDeck = CardDeck.Deck();
-            //foreach (var element in newDeck)
+            //Console.WriteLine("Game Start!");
+            //Console.WriteLine("Select the number of players from 2 to 6!");
+            //int numberOfPlayers = Int32.Parse(Console.ReadLine());
+            //for (int i = 0; i < numberOfPlayers; i++)
             //{
-            //  Console.WriteLine("{0},{1}", element.CardNumber, element.Suit);
-            //    someI++;
+            //    string Name = Console.ReadLine();
+            //    Gamer SomeGamer = new Gamer { GamerName = Name, GamerIndex = i, GamerPoints = 0, Status = GamerStatus.Plays };
             //}
-            //Console.WriteLine(someI+"   *************************************************************");
+            var elementDict = Cards.CardPointDict["Two"];
 
-           
-                var SomeCard = CardDeck.TakeSomeCard(newDeck);
-                var points = CardDeck.CardPoints(SomeCard);
-              
-                
+            var newDeck = CardDeck.Deck();
+            foreach (var element in newDeck)
+            {
+                Console.WriteLine("{0},{1}", element.CardNumber, element.Suit);
+            }
+            var SomeCard = CardDeck.TakeSomeCard(newDeck);
+            var pointsDict = Cards.CardPointDict[SomeCard.CardNumber];
+            Console.WriteLine(pointsDict);
+
+
+
         }
     }
 }
