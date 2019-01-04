8fb2b9e6f85b102bf85be8e5a35109e9e61e6b8e
Евгений Храмов <Evgenkhramov@gmail.com>
2019-01-05 01:12:40 +0200

    InputOutput

---
+++ ConsoleApp18/InputOutput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class InputOutput
+    {
+        public int IntInpunt(int min, int max)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please, min = {0} and max = {1}, enter a valid number ", min, max);
+            if (inputNumber >= min && inputNumber <= max)
+            {
+                return inputNumber;
+            }
+            else
+            {
+                Console.WriteLine("Please, min = {0} and max = {1}, enter a valid number ", min, max);
+                return 0;
+            }
+
+             
+        }
+    }
+}
