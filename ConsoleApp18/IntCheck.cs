17f5cd57fbc93fc54e7a7eb482ae7b98cb228b98
Yvgen Khramov <Evgenkhramov@gmail.com>
2019-01-18 16:45:47 +0200

    chack

---
+++ ConsoleApp18/IntCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackJackProject.Constanta;
+
+namespace BlackJackProject
+{
+    public class IntCheck
+    {
+        private ConsoleOutput output = new ConsoleOutput();
+        public int CheckNumber(int min, int max)
+        {
+            int number = 0;
+            int validNumber = 0;
+            bool valid = true;
+            while (valid)
+            {
+                try
+                {
+                    string someText = Console.ReadLine();
+                    bool success = Int32.TryParse(someText, out number);
+                    if (success && number >= min && number <= max)
+                    {
+                        validNumber = number;
+                        valid = false;
+                    }
+                    if (!success || number < min || number > max)
+                    {
+                        output.ShowSomeOutput(TextCuts.EnterValidNumber, min, max);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine(TextCuts.NotValidNumber);
+                }
+            }
+            return validNumber;
+        }
+    }
+}
